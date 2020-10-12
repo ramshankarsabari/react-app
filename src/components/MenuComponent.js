ebfa1901987e7cca67d33a6b165c36bd792dd3ec
Ramshankar S <13503230+ramshankarsabari@users.noreply.github.com>
2020-10-13 00:44:27 +0530

    components part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,62 +1,51 @@
 import React, { Component} from 'react';
-import { Media } from 'reactstrap';
+import { Card, CardBody, CardImg , CardText, CardTitle, CardImgOverlay } from 'reactstrap';
 
 
 class Menu extends Component {
 
     constructor(props) {
         super(props); // this is reqiured when you define a class component
+        // to store objects/data within a class component use state, which is usually declared inside constructor
+        // cannot directly change this.state data instead use setState() method to set state
+
+        // to pass data to child components use props
         this.state = {
-            dishes: [{
-                    id: 0,
-                    name: 'Uthappizza',
-                    image: 'assets/images/uthappizza.png',
-                    category: 'mains',
-                    label: 'Hot',
-                    price: '4.99',
-                    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-                },
-                {
-                    id: 1,
-                    name: 'Zucchipakoda',
-                    image: 'assets/images/zucchipakoda.png',
-                    category: 'appetizer',
-                    label: '',
-                    price: '1.99',
-                    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-                },
-                {
-                    id: 2,
-                    name: 'Vadonut',
-                    image: 'assets/images/vadonut.png',
-                    category: 'appetizer',
-                    label: 'New',
-                    price: '1.99',
-                    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-                },
-                {
-                    id: 3,
-                    name: 'ElaiCheese Cake',
-                    image: 'assets/images/elaicheesecake.png',
-                    category: 'dessert',
-                    label: '',
-                    price: '2.99',
-                    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-                }
-            ],
+            selectedDish : null
         };
     }
-    render() { // this render() method returns corresponding view of the component
-        const menu = this.state.dishes.map((dish) => { 
+    setSelectedDish(dish){
+        this.setState({ selectedDish: dish});
+    }
+    renderSelectedDish(dish){
+        if(dish != null){
+            return (
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                </Card>
+            );
+        }else{
             return(
-            <div key={dish.id} className="col-12 mt-5">
-            <Media tag='li'>
-                <Media object src={dish.image} alt={dish.name} />
-                    <Media body  className="ml-5">
-                        <Media heading>{dish.name}</Media>
-                        <p>{dish.description}</p>
-                    </Media>
-                </Media>
+                <div></div>
+            );
+        }
+    }
+
+
+    render() { // this render() method returns corresponding view of the component
+        const menu = this.props.dishes.map((dish) => { 
+            return(
+            <div key={dish.id} className="col-12 col-md-5 mt-1">
+            <Card onClick={() => this.setSelectedDish(dish)}>
+                <CardImg width='100%' src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle >{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Card>
             </div>
             );}
         );
@@ -64,11 +53,15 @@ class Menu extends Component {
             // here, we are returning view for the component
             <div className="container">
                 <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
+                    {menu}
+                </div>
+                <div className="row">
+                    <div  className="col-12 col-md-5 m-1">
+                        {this.renderSelectedDish(this.state.selectedDish)}
+                    </div>
                 </div>
             </div>
+            
         );
     }
 }
